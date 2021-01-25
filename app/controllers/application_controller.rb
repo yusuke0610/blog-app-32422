be91b50a34a73b9d51d4d4a44887d3324f4f8414
yusuke0610 <74752096+yusuke0610@users.noreply.github.com>
2021-01-25 19:35:33 +0900

    Basic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,10 +8,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_in,keys: [:email,:password])
   end
 
-  def production?
-    Rails.env.production?
-  end
-  
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
